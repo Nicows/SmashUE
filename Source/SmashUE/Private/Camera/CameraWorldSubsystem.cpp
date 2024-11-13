7a4a5347ad64405adb833fdd094e4e57527aebcd
Nicolas Kohler <nicolas.kohler@edu.devinci.fr>
2024-11-13 11:33:21 +0100

    Camera start

---
+++ Source/SmashUE/Private/Camera/CameraWorldSubsystem.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Camera/CameraWorldSubsystem.h"
+
+void UCameraWorldSubsystem::PostInitialize()
+{
+	Super::PostInitialize();
+}
+
+void UCameraWorldSubsystem::OnWorldBeginPlay(UWorld& InWorld)
+{
+	Super::OnWorldBeginPlay(InWorld);
+	CameraMain = FindCameraByTag(TEXT("CameraMain"));
+}
+
+void UCameraWorldSubsystem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	TickUpdateCameraPosition(DeltaTime);
+}
+
+void UCameraWorldSubsystem::AddFollowTarget(AActor* FollowTarget)
+{
+	
+}
+
+void UCameraWorldSubsystem::RemoveFollowTarget(AActor* FollowTarget)
+{
+	
+}
+
+void UCameraWorldSubsystem::TickUpdateCameraPosition(float DeltaTime)
+{
+	
+}
+
+FVector UCameraWorldSubsystem::CalculateAveragePositionBetweenTargets()
+{
+	return FVector::ZeroVector;
+}
+
+UCameraComponent* UCameraWorldSubsystem::FindCameraByTag(const FName& Tag) const
+{
+	return nullptr;
+}
